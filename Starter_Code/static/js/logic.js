9eb860bc5b2444e53ddcdcc6a1a2c54579a0d4d5
SheTroxel <smtroxel@gmail.com>
2023-04-13 23:25:47 -0400

    Finished code for leaflet part 1

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -3,18 +3,18 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-  console.log.data
+  console.log(data);
   createFeatures(data.features);
 });
 
 // Set color schema for depth/markers
 let color = {
   level1: '#1feb35',
-  level2: '#00ce74',
+  level2: '#00dc5e',
   level3: '#00ae92',
-  level4: '#008c96',
-  level5: '##008c96',
-  level6: '#2a4858'
+  level4: '#00b98b',
+  level5: '#00a695',
+  level6: '#009398'
 
 }
 //console.log(color)
@@ -26,11 +26,8 @@ function createFeatures(earthquakeData) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><br><h2> Magnitude:${feature.properties.mag}</h2>`);
   }
 
-// Set variables for latitude, longitude, depth and magnitude
-var latitude= [];
-var longitude=[];
-var depth=[];
-var magnitude=[];
+// Set marker variables for latitude, longitude, depth and magnitude
+
 var magMarkers = []; //create an array to store magMarkers
 
 for (var i = 0; i < earthquakeData.length; i++) { // Use earthquakeData.length
@@ -70,65 +67,76 @@ for (var i = 0; i < earthquakeData.length; i++) { // Use earthquakeData.length
     fillOpacity: 1,
     weight: 1
   });
-
-  magMarkers.push(magMarker); // add magMarkers to magMarkers array
-}
-//console.log(magMarker)
-L.layerGroup(magMarkers).addTo(myMap); // create a layer group from magMarkers and add it to the map
-}
-
-// Set the legend to appear in the bottom right of our map//
-var legend = L.control({
-  position: 'bottomright'
-});
-
-// Adding on the legend based off the color scheme we have //
-legend.onAdd = function (map) {
-  var div = L.DomUtil.create('div', 'legend');
-  var levels = ['>1', '1-2', '2-3', '3-4', '4-5', '5+'];
-  var colors = ['#1feb35', '#00ce74', '#00ae92','#008c96','##008c96','#2a4858];
   
-  for (var i = 0; i < levels.length; i++) {
-      div.innerHTML += '<i style="background:' + colors[i] + '"></i>' + levels[i] + '<br>';
-  }
-  return div;
+  // add magMarkers to magMarkers array
+  magMarkers.push(magMarker); 
 }
-console.log(legend)
-
 // Create our map, giving it the streetmap and earthquakes layers to display on load.
 var myMap = L.map("map", {
-  setView: [
-    37.09, -95.71
-  ],
+  center: [37.09, -95.71],
   zoom: 5,
   });
 
-  // Create title layer
+//create title layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(myMap);
 
-// Create the base layers.
-var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-})
-
-var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// Create street view and topographic map tile layers
+var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-// Create a baseMaps object.
+var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: '&copy; <a href="https://opentopomap.org/about">OpenTopoMap</a>'
+});
+
+// Create an object to hold the base layers
 var baseMaps = {
-  "Street Map": street,
-  "Topographic Map": topo
-};//
+  "Street Map": streetMap,
+  "Topographic Map": topoMap
+};
 
+// Create an overlay layer for the earthquake markers
+var earthquakeLayer = L.layerGroup(magMarkers);
 
-// add layer to my Map
-L.control.layers(baseMaps).addTo(myMap);
-//console log to confirm code is working
-console.log(baseMaps)
+// Create an object to hold the overlay layers
+var overlayMaps = {
+  "Earthquakes": earthquakeLayer
+};
 
+// Create the map with streetMap as the default tile layer
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [streetMap, earthquakeLayer] // Add streetMap and earthquakeLayer as default layers
+});
 
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
+// Create a legend
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create("div", "info legend");
+  var depthLevels = [-10, 10, 20, 40, 60, 80, 100];
+  var labels = [];
+
+  div.innerHTML += "<h4>Depth (km)</h4>"; // Add legend title
+
+  // Generate labels for the legend based on depth levels and color schema
+  for (var i = 0; i < depthLevels.length; i++) { // Update this line to iterate from 0 to 7
+    div.innerHTML +=
+      '<i style="background:' + getColor(depthLevels[i] + 1) + '"></i> ' +
+      depthLevels[i] + (depthLevels[i + 1] ? '&ndash;' + depthLevels[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+};
+
+// Add the legend to the map
+legend.addTo(myMap)
